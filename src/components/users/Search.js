322b7d35729430dbbcd1cb4557aea74a61f5df04
kyho <kyho1662@gmail.com>
2020-07-06 11:49:25 +0900

    context

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert}) => {
+const Search = () => {
+    const githubContext = useContext(GithubContext);
+    const alertContext = useContext(AlertContext);
+
     const [text, setText] = useState('');
 
     const onSubmit = (e) => {
         e.preventDefault();
         if(text === '') {
-            setAlert('Please enter something', 'light');
+            alertContext.setAlert('Please enter something', 'light');
         } else {
-            searchUsers(text);
+            githubContext.searchUsers(text);
             setText('');
         }
         
@@ -20,14 +24,6 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert}) => {
         setText(e.target.value);
     };
 
-    Search.propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    };
-
-    
 
     return (
         <div>
@@ -37,8 +33,8 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert}) => {
                 <input type="submit" value="Search" 
                 className="btn btn-dark btn-block"/>
             </form>
-            {showClear &&  
-            <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+            {githubContext.users.length > 0 &&  
+            <button className="btn btn-light btn-block" onClick={githubContext.clearUsers}>Clear</button>}
             
         </div>
     );
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,19 +1,22 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+
+const User = ({ match }) => {
+    const githubContext = useContext(GithubContext);
+
+    const { getUser, loading, user, repos, getUserRepos } = githubContext;
+    
     useEffect(() => {
         getUser(match.params.login);
         getUserRepos(match.params.login);
         // eslint-disable-next-line 
-    }, []);
+    }, []);    
 
- 
-    
     const {
         name,
         company,
@@ -29,14 +32,6 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
         public_gists,
         hireable
     } = user;
-
-    User.propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-    };
 
     if(loading) return <Spinner />
     return (
